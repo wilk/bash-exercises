d31aff17dc3f0f336cbc43104bc5491e83de8b7a
Wilk <vincenzo@ibuildings.it>
2017-01-19 10:10:20 +0100

    init repo

---
+++ exercises/file-read.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# dato un file in input, restituire una conferma se l'utente ha i permessi di lettura altrimenti una negazione
+# commands hint: [ -r $file ], if
---
+++ exercises/watch-rows.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# dato un file testuale in input, monitorare il numero di righe: se questo sfora un limite dato, loggare sul log di sistema e terminare
+# commands hint: cut, wc, cat, logger, sleep, while
